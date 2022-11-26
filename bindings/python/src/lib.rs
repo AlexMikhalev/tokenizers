4f86e10db3f5ead0e2f423cb5d251f16858aa19c
AlexMikhalev <alex@metacortex.engineer>
2022-11-26 17:08:59 +0000

    RIP env_logger
    Signed-off-by: AlexMikhalev <alex@metacortex.engineer>

--- bindings/python/src/lib.rs
+++ bindings/python/src/lib.rs
@@ -46,7 +46,6 @@ extern "C" fn child_after_fork() {
 /// Tokenizers Module
 #[pymodule]
 pub fn tokenizers(_py: Python, m: &PyModule) -> PyResult<()> {
-    let _ = env_logger::try_init_from_env("TOKENIZERS_LOG");
 
     // Register the fork callback
     #[cfg(target_family = "unix")]
